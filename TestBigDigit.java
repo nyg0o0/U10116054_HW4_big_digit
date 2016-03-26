7af7d9bcc22725e49548c13534ecfa1427e2fd24
nyg0o0 <yaya56790@gmail.com>
2016-03-26 15:25:34 +0800

    read the input number

--- TestBigDigit.java
+++ TestBigDigit.java
@@ -2,13 +2,25 @@ import java.util.*;
 
 public class TestBigDigit {
   public static void main(String[] args) {
+
     // Prompt user to input a big number
     System.out.print("Pleas enter a big number: ");
     Scanner in = new Scanner(System.in);
-    String str = in.next();
-    System.out.print(str);
-    int intPart = str.indexOf('.');
-    int decimalPart = str.length() - intPart - 1;
-    System.out.print(intPart + " " + decimalPart);
+    String inputStr = in.next();
+
+    // Check for inputStr is a number or not
+    try{ 
+      System.out.print(Double.parseDouble(str));
+    }
+    catch (NumberFormatException e){ 
+      System.out.println("This is not a digit);
+    }
+   
+    // Record the number of digits (before and after decimal point)
+    int intPart = inputStr.indexOf('.');
+    int decimalPart = inputStr.length() - intPart - 1;  // string length - interger part - decimal point
+    System.out.print(intPart + " " + decimalPart); 
+
+    // Store the number into the array
   }
 }
