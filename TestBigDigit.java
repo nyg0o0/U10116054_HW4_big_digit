604e0ad208b7de86e52ca6ae0e7f06ace9074332
nyg0o0 <yaya56790@gmail.com>
2016-03-26 20:47:32 +0800

    modify some errors

--- TestBigDigit.java
+++ TestBigDigit.java
@@ -3,52 +3,45 @@ import java.util.*;
 public class TestBigDigit {
 	public static void main(String[] args) {
 
-		// Prompt user to input a big number
-		System.out.print("Pleas enter a big number: ");
+		/* Prompt user to How many digital operations?(at least two) */
 		Scanner in = new Scanner(System.in);
-		String inputStr = in.next();
-
-		// Check for inputStr is a number or not
-		try{ 
-		  Double.parseDouble(inputStr);
-		}
-		catch (NumberFormatException e){ 
-		  System.out.println("This is not a digit!");
-		  System.exit(1);
-		}
-  
-  
-        /* First, check for its type, and store them into the array */  
-		
-		int intPartNum = 0, decimalPartNum = 0;
-		int[] intPart,decimalPart;
-		if(inputStr.indexOf('.') == -1){	// If it is a interger
-		
-			// Record the number of digits (before and after decimal point)
-			intPartNum = inputStr.length();
-			intPart = new int[intPartNum];
-		
-			// Store the number into the array	
-			for(int j = 0; j < intPartNum ; j++){
-				intPart[intPartNum-j-1] = Character.getNumericValue(inputStr.charAt(j));
-			}	
-		}
-		else{		// If it is not a interger
-			// Record the number of digits (before and after decimal point)
-			intPartNum = inputStr.indexOf('.');
-			decimalPartNum = inputStr.length() - intPartNum - 1;  // string length - interger part - decimal point
-			intPart = new int[intPartNum];
-			decimalPart = new int[decimalPartNum];
-		
-			// Store the intPart into the array	
-			for(int i = 0; i < intPartNum ; i++){
-				intPart[intPartNum-i-1] = Character.getNumericValue(inputStr.charAt(i));
+		int numOfOperation = 0;
+		do{
+			System.out.print("How many digital operations?(at least two) ");
+			try{ 
+				numOfOperation = in.nextInt();
 			}
-			// Store the deciamlPart into the array	
-			for(int j = 0; j < decimalPartNum ; j++){
-				decimalPart[j] = Character.getNumericValue(inputStr.charAt( ( inputStr.indexOf('.') + 1 ) + j));
+			catch (NumberFormatException e){ 
+				System.out.println("Please input a interger!");
 			}
+		}while (numOfOperation < 2);	// if it is smaller than two, propt user to enter again
 
+		String[] inputStr = new String[numOfOperation];		// A input string
+		ProcessInputNumber[] num = new ProcessInputNumber[numOfOperation];	// A object array for class processInputNumber
+		
+		for(int i = 0 ; i < numOfOperation ; i++){
+			/* Prompt user enter datas, and new objects to check datas */
+		    num[i] = new ProcessInputNumber();	// Initial the object array
+			System.out.print("Pleas enter No." + (i+1) +" big number: ");	//Prompt user to enter big number
+			inputStr[i] = in.next();	// Scan the big number
+			num[i].checkIsNumber(inputStr[i]);	// check the input is number or not by using the class processInputNumber
+			
+			/* Check for its type, and store them into the array */
+			if(inputStr[i].indexOf('.') == -1){	// If it is a interger
+				num[i].storeIntNum(inputStr[i]);
+			}
+			else{	// If it is not a interger
+				num[i].storeDecimalNum(inputStr[i]);
+			}
 		}
+		
+		//for(int j = 0 ; j < numOfOperation ; j++){
+			DoOperation operation1 = new DoOperation(num[0].intPart,num[0].decimalPart,num[1].intPart,num[1].decimalPart);
+			operation1.doAddition();
+			System.out.println(operation1.getResult_IntPartNum());
+			for(int j = 0 ; j < operation1.result_IntPartNum ; j++){
+				System.out.println(operation1.result_IntPart[j]);
+			}
+		//}
 	}
 }
