7d381ab37fb5241582767205d639765924b1b083
nyg0o0 <yaya56790@gmail.com>
2016-03-26 17:28:42 +0800

    separte the programthem into two class

--- BigDigit.java
+++ BigDigit.java
@@ -1,5 +1,35 @@
 import java.util.*;
 
-public class BigDigit{
+class BigDigit{
+	private int intPartNum = 0, decimalPartNum = 0;
+	private int[] intPart,decimalPart;	
+	BigDigit(){
+		
+		
+	}
+	
 
+
+	public void checkIsNumber(String newStr){
+		// Check for inputStr is a number or not
+		try{ 
+		  Double.parseDouble(newStr);
+		}
+		catch (NumberFormatException e){ 
+		  System.out.println("This is not a digit!");
+			 System.exit(1);
+		}
+	}
+	
+	public void storeIntNum(String newStr){
+		// Record the number of digits (before and after decimal point)
+		intPartNum = newStr.length();
+		intPart = new int[intPartNum];
+		
+		// Store the number into the array	
+		for(int j = 0; j < intPartNum ; j++){
+			intPart[intPartNum-j-1] = Character.getNumericValue(newStr.charAt(j));
+		}	
+	}
 }
+
